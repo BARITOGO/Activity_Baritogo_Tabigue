4a0374d065a5960c0013727bf8468db64db2ec65
Asus <Asus@LAPTOP-DQ5O1KFR>
2024-10-01 19:09:16 +0800

    Activity_Baritogo_Tabigue_Lustre

--- src/activity_baritogo_tabigue_lustre/Activity_Baritogo_Tabigue_Lustre.java
+++ src/activity_baritogo_tabigue_lustre/Activity_Baritogo_Tabigue_Lustre.java
@@ -1,19 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package activity_baritogo_tabigue_lustre;
 
-/**
- *
- * @author Asus
- */
+import java.util.Scanner;
 public class Activity_Baritogo_Tabigue_Lustre {
 
    
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("World");
-    }
+        
+         
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num2 != 0 ? num1 / num2 : Double.NaN; 
+        
+        
+        System.out.println("Results:");
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        
+        
+        scanner.close();
+    }    
+    
     
 }
