0eb3ad9de49da419d3c1e81290ea99008b9c0832
Asus <Asus@LAPTOP-DQ5O1KFR>
2024-10-01 18:20:39 +0800

    Activity_Baritogo_Tabigue_Lustre

--- src/activity_baritogo_tabigue/Activity_Baritogo_Tabigue.java
+++ src/activity_baritogo_tabigue/Activity_Baritogo_Tabigue.java
@@ -10,11 +10,9 @@ package activity_baritogo_tabigue;
  */
 public class Activity_Baritogo_Tabigue {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Iyot");
     }
     
 }
