5bec5423e531c5eee4a67517c4ae22a34f8a9fa1
Asus <Asus@LAPTOP-DQ5O1KFR>
2024-10-01 18:22:06 +0800

    Activity_Baritogo_Tabigue_Lustre

--- src/activity_baritogo_tabigue_lustre/Activity_Baritogo_Tabigue_Lustre.java
+++ src/activity_baritogo_tabigue_lustre/Activity_Baritogo_Tabigue_Lustre.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package activity_baritogo_tabigue;
+package activity_baritogo_tabigue_lustre;
 
 /**
  *
  * @author Asus
  */
-public class Activity_Baritogo_Tabigue {
+public class Activity_Baritogo_Tabigue_Lustre {
 
    
     public static void main(String[] args) {
